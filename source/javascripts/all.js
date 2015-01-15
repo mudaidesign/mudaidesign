50b5424bbaf71a682de65f0fc0bf91158af8275d
Arvin Dang <git@arvindang.com>
2015-01-15 16:36:00 -0600

    Updated title, added meta tags for future use, added padding for headings to line up with grid elements

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+$(function() {
+	$('.how').hover(function() {
+		$('body').toggleClass('blue');
+	});
+
+	$('.join').hover(function() {
+		$('body').toggleClass('green');
+	});
+});
