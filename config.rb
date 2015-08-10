236be4dd67f67ba03c92ac1b4775dd3b678eeb10
Arvin Dang <git@arvindang.com>
2015-08-10 08:54:47 -0500

    adding build files

--- config.rb
+++ config.rb
@@ -50,12 +50,12 @@
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-activate :relative_assets
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
+	# activate :relative_assets
+  # activate :minify_css
 
   # Minify Javascript on build
   activate :minify_javascript
